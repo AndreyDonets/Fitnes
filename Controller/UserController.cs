b09f25349e02310a709e8baa0401a2dd84b9a631
AndreyDonets <68755934+AndreyDonets@users.noreply.github.com>
2021-05-11 20:39:10 +0300

    Добавьте файлы проекта.

---
+++ Controller/UserController.cs
@@ -0,0 +1,29 @@
+﻿using FitnesBL.Model;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FitnesBL.Controller
+{
+    public class UserController
+    {
+        public User User { get; }
+        public UserController(string userName, string genderName, DateTime birthDate, double weight, double height)
+        {
+            User = new User(userName, new Gender(genderName), birthDate, weight, height);
+        }
+        public UserController()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+                if (formatter.Deserialize(fs) is User user)
+                    User = user;
+        }
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+                formatter.Serialize(fs, User);
+        }
+    }
+}
